06d9c369c1b1774e698e77f94c23339cb416eac7
Adriel  Torquato <adryeltc2001@gmail.com>
2022-12-27 16:32:25 -0300

    exercicio1
    fiz tudo legalzinho

--- Aula12/ex014/script.js
+++ Aula12/ex014/script.js
@@ -1,1 +1,22 @@
-alert('Olá, chifrudo')
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 7
+    msg.innerHTML = `Agora são ${hora} horas!`
+    if (hora >= 18|| hora < 6){
+        //BOA NOITE
+        img.src = "fotonoite.png"
+        document.body.style.background = "#16263E"
+    } else if (hora < 12){
+        //BOM DIA
+        img.src = "fotomanha.png"
+        document.body.style.background = "#9EA071"
+    } else{
+        //BOA TARDE
+        img.src = "fototarde.png"
+        document.body.style.background = "#A5816D"
+
+    }
+}
